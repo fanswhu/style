42114aa9109832c9ea40b092400bfe24b60b3729
feng <feng>
2018-03-09 17:42:34 +0800

    add

---
+++ app/src/main/java/com/axmf/jttest/CcbApp.java
@@ -0,0 +1,14 @@
+package com.axmf.jttest;
+
+import android.app.Application;
+
+/**
+ * Created by feng on 2018/3/9.
+ */
+
+public class CcbApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
