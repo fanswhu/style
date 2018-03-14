02aba883244f0e0aa2d560d63d44c50d01d1686f
feng <feng>
2018-03-14 14:50:00 +0800

    api

--- app/src/main/java/com/axmf/jttest/Api.java
+++ app/src/main/java/com/axmf/jttest/Api.java
@@ -1,8 +1,15 @@
 package com.axmf.jttest;
 
+import retrofit2.http.GET;
+import rx.Observable;
+
 /**
  * Created by feng on 2018/3/13.
  */
 
-public class Api {
+public interface Api {
+    /*@GET("/student/login")
+    Observable<HttpResult> login(@Query("phone") String phone, @Query("password") String psw);*/
+    @GET("article/today?dev=1")
+    Observable<String> today();
 }
