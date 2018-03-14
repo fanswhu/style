760aacb71946e60d15bf2ec7c2f3a94ed7179db6
feng <feng>
2018-03-14 14:51:13 +0800

    api

--- app/src/main/java/com/axmf/jttest/MainActivity.java
+++ app/src/main/java/com/axmf/jttest/MainActivity.java
@@ -1,6 +1,5 @@
 package com.axmf.jttest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +7,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et = (EditText) findViewById(R.id.question);
         tv = (TextView) findViewById(R.id.answer);
-      for(int i = 0;i<100;i++){
-          Handler handler = new Handler();
-      }
 
     }
 
     public  void request(View v){
-        startActivity(new Intent(this,LeakActivity.class));
+        ApiService.service.today()
+        .subscribeOn(Schedulers.io())
+        .unsubscribeOn(Schedulers.io())
+        .subscribeOn(AndroidSchedulers.mainThread())
+        .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.e("----","完成");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("----","错误");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.e("---",s);
+                    }
+                });
     }
 
-    @Override
-    protected void onDestroy() {
-
-    }
 }
